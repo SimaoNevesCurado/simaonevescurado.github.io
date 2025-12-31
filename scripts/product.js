5783fa282211e7022c28a9e5941b88bc8c19e5ed
simaonevescurado <simao.curadoo@gmail.com>
2025-12-31 17:32:18 +0000

    test

--- scripts/product.js
+++ scripts/product.js
@@ -1,5 +1,6 @@
-import * as THREE from 'three';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import * as THREE from 'https://unpkg.com/three@0.158.0/build/three.module.js';
+import { GLTFLoader } from 'https://unpkg.com/three@0.158.0/examples/jsm/loaders/GLTFLoader.js';
+
 
 // CENA + RENDERER
 const cena = new THREE.Scene();
